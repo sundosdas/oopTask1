387d843ed03768c581f9e85804299f1543bd602e
DASOUQI <DASOUQI@HCR-PC1RG8J8>
2024-11-13 22:09:30 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopTask1
+{
+    public interface IEngine
+    {
+        void Start();
+    }
+
+    public class Engine : IEngine
+    {
+        public void Start()
+        {
+            Console.WriteLine("Engine Started");
+        }
+    }
+
+    public class Car
+    {
+        private IEngine eng;
+        public Car(IEngine engine)
+        {
+            this.eng = engine;
+        }
+        public void Start() 
+        {
+            eng.Start();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEngine engine = new Engine();
+            Car car = new Car(engine);
+            car.Start();    
+        }
+    }
+}
